1a3e61708b5b05da32e632f8612af89f8313eff7
Nathaniel J. Liberty <nathaniel.liberty@gmail.com>
2020-01-06 17:41:37 -0500

    Correct static asset path
    Also adds alert with directions for use.

--- loaders/middleware.js
+++ loaders/middleware.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const express = require('express');
 
 module.exports = app => {
-  app.use(express.static('/client/dist'));
+  app.use(express.static('client/dist'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cors());
